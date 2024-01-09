1d882a889449bbf4c811a4e9edcc226a2482064f
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-01-09 19:33:44 +0530

    logger added

---
+++ .history/main_20240109192749.py
@@ -0,0 +1,3 @@
+from src.mlProject.logging import logger
+
+logger.info("this is out custom log!")
--- .history/src/mlProject/logging/__init___20240109191301.py
+++ .history/src/mlProject/logging/__init___20240109191301.py
@@ -7,17 +7,16 @@ logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 log_dir = "logs"
 
 log_filepath = os.path.join(log_dir, "running_log.log")
-os.makedirs(log_dir, exist_ok=True)
+os.makedirs(log_dir, exit_ok=True)
 
 
 logging.basicConfig(
     level = logging.INFO,
-    format = logging_str,
+    format = logging_str
 
-    handlers=[
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
-    ]
+    handlers=[datefmt=logging.FileHandler(log_filepath),
+              logging.StreamHandler(sys.stdout)
+              ]
 )
 
-logger = logging.getLogger("mlProjectLogger")
+logger = logging.getlogger("mlProjectLogger")

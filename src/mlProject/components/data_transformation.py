e9961a71f1b00888de71f97d994ae806bdef1bdf
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-01-10 23:32:11 +0530

    data transformation updated

---
+++ src/mlProject/components/data_transformation.py
@@ -0,0 +1,8 @@
+import os
+from mlProject import logger
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from mlProject.entity.config_entity import DataTransformationConfig
+
+
+
